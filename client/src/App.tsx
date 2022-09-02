20aab85d23838d23aabcbe7372e888edcd077dcb
Mohammed <mohammednady128@gmail.com>
2022-09-02 14:51:52 +0200

    create LayoutRoute and axios error handler

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,25 @@
-import React, { Fragment, useMemo } from "react"
+import { useMemo } from "react"
 import { createTheme, ThemeProvider } from "@mui/material/styles"
 import { CssBaseline, useMediaQuery } from "@mui/material"
 import Login from "./pages/Login"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom"
 import Register from "./pages/Register"
 import ProtectedRoute from "./components/ProtectedRoute"
 import Home from "./pages/Home"
 import { ChatProvider } from "./context/chatContext"
 import { SocketProvider } from "./context/socketContext"
+
+function Main(){
+    return <>
+        <ChatProvider>
+            <SocketProvider>
+            <ProtectedRoute>
+                <Outlet/>
+                </ProtectedRoute>
+            </SocketProvider>
+        </ChatProvider>
+    </>
+}
 
 function App() {
     const prefersDarkMode = useMediaQuery('(prefers-color-schema: dark)')
@@ -23,14 +35,10 @@ function App() {
                 <Routes>
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
+                    <Route element={<Main/>}>
+                        <Route path="/" element={<Home/>}/>
+                    </Route>
                 </Routes>
-                <ChatProvider>
-                    <SocketProvider>
-                        <Routes>
-                            <Route path="/" element={<ProtectedRoute><Home/></ProtectedRoute>}/>
-                        </Routes>
-                    </SocketProvider>
-                </ChatProvider>
             </BrowserRouter>
         </ThemeProvider>
     </>
--- client/src/axios.ts
+++ client/src/axios.ts
@@ -2,5 +2,13 @@ import axios from "axios";
 
 const axiosClient = axios.create({baseURL:"http://localhost:8000/",withCredentials:true})
 
+axiosClient.interceptors.response.use((response)=>response,(error)=>{
+    if(axios.isAxiosError(error)){
+        if(error.response?.status===401){
+            window.location.href = "/login";
+        }
+    }
+})
+
 
 export default axiosClient;
